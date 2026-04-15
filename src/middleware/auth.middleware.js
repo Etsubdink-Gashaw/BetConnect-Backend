1435adeb29849f1d03d7e9e158795e77f3ebe9f9
yarbework <yarbemohaz3517@gmail.com>
2026-04-16 01:33:51 +0300

    creating middleware autorization middleware to protect the routes for authorized user

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,54 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.model.js';
+
+export const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+
+export const optionalAuth = async (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+    } catch (error) {
+      req.user = null;
+    }
+  } else {
+    req.user = null;
+  }
+  next();
+};
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ message: 'Not authorized' });
+  }
+};
+
+
+export const approvedAgent = (req, res, next) => {
+  if (req.user && req.user.role === 'agent' && req.user.status === 'approved') {
+    next();
+  } else {
+    res.status(403).json({ message: 'Not authorized Agent' });
+  }
+};
