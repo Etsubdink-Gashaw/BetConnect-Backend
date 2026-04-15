247a442f508015f0ba176cc6b0e49f3995c5b61c
yarbework <yarbemohaz3517@gmail.com>
2026-04-16 01:14:51 +0300

    creating auth controller and auth routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
-app.get('/haelth', (req, res) => {
+app.get('/health', (req, res) => {
     res.status(200).json({ 
         status: 'success',
         message: 'Server is healthy' });
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,71 @@
+import User from '../models/User.model';
+import generateToken from '../utils/generateToken';
+
+export const register = async (req, res) => {
+    try{
+
+        const {name, email, phone, password, role, personalAddress} = req.body;
+        
+        if(!name || !email || !phone || !password || !role || !personalAddress){
+            console.log("Please insert all the required fields")
+        }
+        const userExists = await User.findOne({email}); 
+        if(userExists){
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        const assignedRole = role === 'agent' ? 'agent' : 'user';
+
+        const assignedStatus = assignedRole === 'agent' ? 'pending' : 'approved';
+
+        const user = await User.create({
+            name,
+            email,
+            phone,
+            password,
+            role: assignedRole,
+            status: assignedStatus,
+            personalAddress: assignedRole === 'agent' ? personalAddress : undefined,
+        });
+
+        if(user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name, 
+                email: user.email,
+                role: user.role,
+                status: user.status,
+                token: generateToken(user._id),
+            });
+        }else {
+            res.status(400).json({
+                message: 'Invalid user data'
+            });
+        }
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        
+        const user = await User.findOne({email});
+        if(user && (await user.matchPassword(password))){
+            res.json({
+                _id: user._id,
+                name: user.name, 
+                email: user.email,
+                role: user.role,
+                status: user.status,
+                token: generateToken(user._id),
+            });
+        }else {
+            res.status(401).json({message: 'Invalid email or Passowrd'});
+        }
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+};
